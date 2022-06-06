1b018b5f4b7720483840b6e05cf9569375b6c829
matheus caldeira <gamesmatheusplay9@gmail.com>
2022-06-06 11:34:56 -0300

    commiit

--- api/src/controller/petController.js
+++ api/src/controller/petController.js
@@ -1,7 +1,15 @@
 import { Router } from "express";
+import { consultarTodos } from "../repository/petRepository";
 const server = Router();
 
+server.get('/pets' , async () => (req,resp), ()=> {
+    const consolutar = consultarTodos().req.body
 
+
+} )
+
+server.post('/pet/nome'), async 
 
 
 export default server
+
--- api/src/repository/petRepository.js
+++ api/src/repository/petRepository.js
@@ -1,8 +1,20 @@
 import {con} from './connection.js'
 
 export async function consultarTodos(id){
+    const comando =
     `
     select id_pet = id,
     nm_filme = nome
     from tb_pet ` ;
+    const resposta =  await con.query(comando,[id]);
+    return resposta
+}
+
+export async function inserirNome(nome){
+    const comando =
+    `insert tb_pet (nm_pet) 
+     values ('')`
+  ;
+    const resposta =  await con.query(comando,[nome]);
+    return resposta
 }
